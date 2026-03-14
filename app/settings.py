6c770157797322b835d0b6a8c4d4b597436cc16a
FishTime1 <cevikhilal16@gmail.com>
2026-03-14 12:47:03 +0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -5,8 +5,11 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET: str
     ADMIN_KEY: str = "4e1ace7667"
+    ADMIN_WEB_USERNAME: str = "fishtimeadmincan"
+    ADMIN_WEB_PASSWORD: str = "4e1ace7667"
     DEVICE_LIMIT: int = 3
     TOKEN_EXPIRE_MINUTES: int = 1440
+    ADMIN_TOKEN_EXPIRE_MINUTES: int = 10080
 
 
 settings = Settings()
